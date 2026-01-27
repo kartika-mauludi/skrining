b61a0642ca24f7a4386c21fc12e07b443dd9f73a
ludius01 <69234573+ludius01@users.noreply.github.com>
2026-01-27 20:59:18 +0700

    route fix

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\AngketController;
 
 
 
